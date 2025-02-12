75a493bd1c2adc164aec2af0fe1f5665a90fd698
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-02-12 15:07:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import React from 'react';
 
+function Child({ isLoggedIn, setIsLoggedIn }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  if (isLoggedIn) {
+    return <h2>Welcome, you are logged in!</h2>;
+  } else {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
+
+export default Child;
 
 ReactDOM.render(<App />, document.getElementById("root"));
