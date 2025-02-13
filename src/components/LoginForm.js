d76f583d7f868bc260fa8aeb98383ef5fd10bc8a
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-02-13 18:09:08 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,1 +1,23 @@
 
+import React from 'react';
+
+function Child({ isLoggedIn, setIsLoggedIn }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  if (isLoggedIn) {
+    return <h2>Welcome, you are logged in!</h2>;
+  } else {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
+
+export default index;
