92b199a7804927695d2a3c2340f4e1e3a3f0f06e
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-05 11:36:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import React from 'react';
+
 
 function Child({ isLoggedIn, setIsLoggedIn }) {
   const handleSubmit = (event) => {
