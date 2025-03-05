5faa46d207a87a20526d2650cd4f9764218721e2
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-05 11:45:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,5 @@ import App from "./components/App";
 
 
 
-function Child({ isLoggedIn, setIsLoggedIn }) {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsLoggedIn(true);
-  };
-
-  if (isLoggedIn) {
-    return <h2>Welcome, you are logged in!</h2>;
-  } else {
-    return (
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-}
-
-export default index;
 
 ReactDOM.render(<App />, document.getElementById("root"));
